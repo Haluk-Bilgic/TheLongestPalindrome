1c6a64b4ab10a858806dd7d3c07c1dd2fe21b2b2
Haluk-Bilgic <ahbilgic1994@gmail.com>
2022-04-25 09:42:47 +0300

    added comments

--- FindLongestPalindrome.java
+++ FindLongestPalindrome.java
@@ -10,6 +10,12 @@ public class FindLongestPalindrome {
 
     public static String expand(String s, int low, int high) {
 
+        /*Equality is checked starting from
+          the center moving one character left and one character
+          right from the center. If both chars are equal then the
+          next set of chars are checked.*/
+
+
         while (low >= 0 && high < s.length()
                 && s.charAt(low) == s.charAt(high)) {
             low--;
@@ -19,16 +25,16 @@ public class FindLongestPalindrome {
     }
 
     public static String longestPalindromeString(String s) {
-        if (s.length() == 0) {
+        if (s.length() == 0) {   // Validation
             return null;
         }
         String longest = s.substring(0, 1);
         for (int i = 0; i < s.length() - 1; i++) {
-
+            // get longest palindrome with odd (center is i)
             String palindrome = expand(s, i, i);
             if (palindrome.length() > longest.length()) {
                 longest = palindrome;
-            }
+            }// get longest palindrome with two numbers (center is i, i+1)
             palindrome = expand(s, i, i + 1);
             if (palindrome.length() > longest.length()) {
                 longest = palindrome;
